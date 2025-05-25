ebc736be1462024676485a48c2f67aa715fde5c6
AadiJain558 <aadijain558@gmail.com>
2025-05-25 08:36:06 +0530

    add webRTC based interactive whiteboard

---
+++ backend/whiteboard.js
@@ -0,0 +1,27 @@
+export class WhiteboardManager {
+  constructor(io) {
+    this.io = io;
+    this.boards = new Map();
+  }
+  handleConnection(socket) {
+    let currentRoom = null;
+    socket.on("whiteboardJoin", ({ roomId }) => {
+      currentRoom = roomId;
+      socket.join(roomId);
+      if (!this.boards.has(roomId)) {
+        this.boards.set(roomId, []);
+      }
+      socket.emit("whiteboardSync", this.boards.get(roomId));
+    });
+
+    socket.on("whiteboardDraw", ({ roomId, stroke }) => {
+      if (!this.boards.has(roomId)) return;
+      this.boards.get(roomId).push(stroke);
+      socket.to(roomId).emit("whiteboardDraw", stroke);
+    });
+
+
+    socket.on("disconnect", () => {
+    });
+  }
+}
